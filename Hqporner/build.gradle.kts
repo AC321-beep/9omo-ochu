0b2020029c87dc7f825ccf794f32e533788d80df
AC321-beep <charanac4@gmail.com>
2026-06-21 11:56:49 +0530

    Update build.gradle.kts

--- Hqporner/build.gradle.kts
+++ Hqporner/build.gradle.kts
@@ -4,14 +4,7 @@ cloudstream {
     status = 1
     tvTypes = listOf("NSFW")
     language = "en"
+    version = 1
 }
 
-android {
-    namespace = "com.hqporner"
-}
 
-dependencies {
-    // Explicitly add the API so it's on the compile classpath
-    implementation("com.lagradost:cloudstream3:pre-release")
-    implementation("org.jsoup:jsoup:1.15.3")   // jsoup already provided by root, but safe to repeat
-}
