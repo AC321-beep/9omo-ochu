29a7f2f9a4bd337806154e7cc4150eb32bada6f6
AC321-beep <charanac4@gmail.com>
2026-06-21 11:44:32 +0530

    Update build.gradle.kts

--- Hqporner/build.gradle.kts
+++ Hqporner/build.gradle.kts
@@ -1,27 +1,12 @@
-plugins {
-    id("com.android.library")
-    id("kotlin-android")
-    id("com.lagradost.cloudstream3.gradle")
-}
-
 cloudstream {
     description = "HQPorner.com NSFW extension"
     authors = listOf("AC321-beep")
     status = 1
     tvTypes = listOf("NSFW")
     language = "en"
+    version = 1
 }
 
 android {
-    compileSdk = 33
-    namespace = "com.hqporner"
-    defaultConfig {
-        minSdk = 21
-        targetSdk = 33
-    }
-}
-
-dependencies {
-    implementation("com.lagradost:cloudstream3:pre-release")
-    implementation("org.jsoup:jsoup:1.15.3")
+    namespace = "com.hqporner"   // Must match your package
 }
