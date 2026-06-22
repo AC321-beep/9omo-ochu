5dd428ddbc1ac60dd9d82f7ffbf447fd5e3c1f99
AC321-beep <charanac4@gmail.com>
2026-06-22 23:46:44 +0530

    Update PerverzijaPlugin.kt

--- Perverzija/src/main/kotlin/com/perverzija/PerverzijaPlugin.kt
+++ Perverzija/src/main/kotlin/com/perverzija/PerverzijaPlugin.kt
@@ -8,6 +8,6 @@ import com.lagradost.cloudstream3.plugins.Plugin
 class PerverzijaPlugin : Plugin() {
     override fun load(context: Context) {
         registerMainAPI(Perverzija())
-        registerExtractorAPI(Extractor())   // ← class name changed
+        registerExtractorAPI(Xtremestream())
     }
 }
