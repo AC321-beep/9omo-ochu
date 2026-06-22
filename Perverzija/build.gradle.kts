2af8938c6c7ca6e9082132c51631740a4ae25fe0
AC321-beep <charanac4@gmail.com>
2026-06-22 23:32:59 +0530

    Update build.gradle.kts

--- Perverzija/build.gradle.kts
+++ Perverzija/build.gradle.kts
@@ -1,9 +1,3 @@
-plugins {
-    id("com.android.library")
-    id("kotlin-android")
-    id("com.lagradost.cloudstream3.gradle")
-}
-
 cloudstream {
     description = "Contains all the videos from Perverzija"
     authors = listOf("AC321-beep")   // only this changed
@@ -11,5 +5,5 @@ cloudstream {
     tvTypes = listOf("NSFW")
     iconUrl = "https://www.google.com/s2/favicons?domain=tube.perverzija.com&sz=%size%"
     language = "en"
-    version = 1
+    version = 2
 }
