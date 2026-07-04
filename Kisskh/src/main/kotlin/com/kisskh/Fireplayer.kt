d2a9b74073671b44d9393fb0687551cbc59359bf
AC321-beep <charanac4@gmail.com>
2026-07-05 00:08:34 +0530

    Update and rename SubDecryptor.kt to Fireplayer.kt

---
+++ Kisskh/src/main/kotlin/com/kisskh/Fireplayer.kt
@@ -0,0 +1,64 @@
+package com.familypornhd
+
+import android.util.Log
+import com.lagradost.cloudstream3.utils.*
+import com.fasterxml.jackson.annotation.JsonProperty
+
+class Fireplayer : ExtractorApi() {
+    override var name = "Fireplayer"
+    override var mainUrl = "https://watchstreamhd.com"
+    override val requiresReferer = true
+
+    override suspend fun getUrl(
+        url: String,
+        referer: String?,
+        subtitleCallback: (SubtitleFile) -> Unit,
+        callback: (ExtractorLink) -> Unit
+    ) {
+        val videoid = url.substringAfter("/video/").substringBefore("?")
+        Log.d("Fireplayer", "Video ID: $videoid")
+
+        FamilyPorn.getDocument(url, referer = referer)  // get cookies
+        val cookies = FamilyPorn.getCookies(url)
+
+        val posturl = "$mainUrl/player/index.php?data=$videoid&do=getVideo"
+        val postdata = mapOf("hash" to videoid, "r" to (referer ?: ""))
+        val headers = mapOf(
+            "Accept" to "*/*",
+            "X-Requested-With" to "XMLHttpRequest",
+            "Referer" to url,
+            "Origin" to mainUrl,
+            "Content-Type" to "application/x-www-form-urlencoded; charset=UTF-8"
+        )
+
+        val response = FamilyPorn.postText(
+            url = posturl,
+            data = postdata,
+            headers = headers,
+            cookies = cookies,
+            referer = url
+        )
+
+        val json = mapper.readValue(response, FireResponse::class.java)
+        val videolink = json.securedlink ?: json.videosource
+
+        if (videolink != null) {
+            callback(
+                newExtractorLink(
+                    source = this.name,
+                    name = this.name,
+                    url = videolink,
+                    type = ExtractorLinkType.M3U8
+                ) {
+                    this.referer = "$mainUrl/"
+                    this.headers = mapOf("Origin" to mainUrl)
+                }
+            )
+        }
+    }
+}
+
+data class FireResponse(
+    @JsonProperty("securedLink") val securedlink: String? = null,
+    @JsonProperty("videoSource") val videosource: String? = null
+)
