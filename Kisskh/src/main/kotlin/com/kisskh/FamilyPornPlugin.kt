960d4272658e6380b62946cf0b5e5eaa99cfb6cb
AC321-beep <charanac4@gmail.com>
2026-07-05 00:13:37 +0530

    Update and rename KisskhPlugin.kt to FamilyPornPlugin.kt

---
+++ Kisskh/src/main/kotlin/com/kisskh/FamilyPornPlugin.kt
@@ -0,0 +1,15 @@
+package com.familypornhd
+
+import android.content.Context
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+
+@CloudstreamPlugin
+class FamilyPornPlugin : Plugin() {
+    override fun load(context: Context) {
+        registerMainAPI(FamilyPorn())
+        registerExtractorAPI(Fireplayer())
+        registerExtractorAPI(VideoStreamingWorld())
+        registerExtractorAPI(BestWish())
+    }
+}
