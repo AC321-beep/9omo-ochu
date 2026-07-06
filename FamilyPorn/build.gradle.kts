b747e126dcab221726b32ae8e89c7a27a64fc8d1
AC321-beep <charanac4@gmail.com>
2026-07-06 21:41:30 +0530

    Update build.gradle.kts

--- FamilyPorn/build.gradle.kts
+++ FamilyPorn/build.gradle.kts
@@ -13,7 +13,7 @@ cloudstream {
     description = "FamilyPornHD It's a natural mix of the neighborhood stepmoms, older stepdads, barely legal stepsisters, and even the college going stepbrother falling into some serious family fun."
     status      = 1              // 1 = working, 3 = broken
     tvTypes     = listOf("NSFW")
-    version     = 1
+    version     = 2
     iconUrl     = "https://www.google.com/s2/favicons?domain=familypornhd.com&sz=%size%"
 }
 
