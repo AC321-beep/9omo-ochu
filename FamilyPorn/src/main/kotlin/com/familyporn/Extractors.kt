91941fb6f103f922724dc43fd5e03e43ead4d90b
AC321-beep <charanac4@gmail.com>
2026-08-30 21:44:10 +0530

    Update and rename FamilyPornExtractor.kt to Extractors.kt

--- FamilyPorn/src/main/kotlin/com/familyporn/Extractors.kt
+++ FamilyPorn/src/main/kotlin/com/familyporn/Extractors.kt
@@ -1,17 +1,13 @@
 package com.familyporn
 
 import com.lagradost.cloudstream3.SubtitleFile
-import com.lagradost.cloudstream3.app
 import com.lagradost.cloudstream3.utils.*
 import com.fasterxml.jackson.annotation.JsonProperty
-import kotlinx.coroutines.delay
 
 class FamilyPornExtractor : ExtractorApi() {
     override var name = "FamilyPornExtractor"
     override var mainUrl = "https://familypornhd.com"
     override val requiresReferer = true
-
-    private val cfInterceptor = FamilyPornInterceptor()
 
     override suspend fun getUrl(url: String, referer: String?): List<ExtractorLink> {
         val links = mutableListOf<ExtractorLink>()
@@ -44,12 +40,8 @@ class FamilyPornExtractor : ExtractorApi() {
             "Content-Type" to "application/x-www-form-urlencoded; charset=UTF-8"
         )
         
-        var response = app.post(url = posturl, data = mapOf("hash" to videoid, "r" to (referer ?: "")), headers = headers, interceptor = cfInterceptor).text
-        if (response.isBlank() || response == "[]" || response == "{}") {
-            delay(2000)
-            response = app.post(url = posturl, data = mapOf("hash" to videoid, "r" to (referer ?: "")), headers = headers, interceptor = cfInterceptor).text
-        }
-        
+        // Uses the centralized safe API call
+        val response = FamilyPornProvider.appPost(url = posturl, data = mapOf("hash" to videoid, "r" to (referer ?: "")), headers = headers).text
         val json = AppUtils.parseJson<FireResponse>(response)
         val link = json.securedlink ?: json.videosource
         if (link != null) {
@@ -72,12 +64,7 @@ class FamilyPornExtractor : ExtractorApi() {
             "Content-Type" to "application/x-www-form-urlencoded; charset=UTF-8"
         )
         
-        var response = app.post(url = posturl, data = emptyMap<String, String>(), headers = headers, interceptor = cfInterceptor).text
-        if (response.isBlank() || response == "[]" || response == "{}") {
-            delay(2000)
-            response = app.post(url = posturl, data = emptyMap<String, String>(), headers = headers, interceptor = cfInterceptor).text
-        }
-        
+        val response = FamilyPornProvider.appPost(url = posturl, data = emptyMap(), headers = headers).text
         val video = AppUtils.parseJson<Video>(response)
         callback(newExtractorLink(source = "VideoStreamingWorld", name = "VideoStreamingWorld", url = video.videoSource, type = ExtractorLinkType.M3U8) {
             this.referer = "https://videostreamingworld.com/"
@@ -95,12 +82,7 @@ class FamilyPornExtractor : ExtractorApi() {
             "Referer" to url
         )
         
-        var response = app.get(url = getUrl, headers = headers, interceptor = cfInterceptor).text
-        if (response.isBlank() || response == "[]" || response == "{}") {
-            delay(2000)
-            response = app.get(url = getUrl, headers = headers, interceptor = cfInterceptor).text
-        }
-        
+        val response = FamilyPornProvider.appGet(url = getUrl, headers = headers).text
         val stream = AppUtils.parseJson<Stream>(response)
         callback(newExtractorLink(source = "BestWish", name = "BestWish", url = stream.streaming_url, type = ExtractorLinkType.M3U8) {
             this.referer = "https://bestwish.lol/"
@@ -111,7 +93,6 @@ class FamilyPornExtractor : ExtractorApi() {
         @JsonProperty("securedLink") val securedlink: String? = null,
         @JsonProperty("videoSource") val videosource: String? = null
     )
-    
     data class Video(val videoSource: String)
     data class Stream(val streaming_url: String)
 }
