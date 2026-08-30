d4fe2d587ff0d5a096e3b2637c96c7adbfeb1a05
AC321-beep <charanac4@gmail.com>
2026-08-30 21:53:21 +0530

    Update FamilyPornPlugin.kt

--- FamilyPorn/src/main/kotlin/com/familyporn/FamilyPornPlugin.kt
+++ FamilyPorn/src/main/kotlin/com/familyporn/FamilyPornPlugin.kt
@@ -7,7 +7,6 @@ import com.lagradost.cloudstream3.plugins.Plugin
 @CloudstreamPlugin
 class FamilyPornPlugin : Plugin() {
     override fun load(context: Context) {
-        // Registering the provider and extractor
         registerMainAPI(FamilyPornProvider())
         registerExtractorAPI(FamilyPornExtractor())
     }
