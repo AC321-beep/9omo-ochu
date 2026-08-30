5f4dc8bd0c8ed1060687852482a77a8d87117f8d
AC321-beep <charanac4@gmail.com>
2026-08-30 21:41:05 +0530

    Update FamilyPornPlugin.kt

--- FamilyPorn/src/main/kotlin/com/familyporn/FamilyPornPlugin.kt
+++ FamilyPorn/src/main/kotlin/com/familyporn/FamilyPornPlugin.kt
@@ -1,36 +1,14 @@
 package com.familyporn
 
 import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
-import com.lagradost.cloudstream3.CloudStreamApp.Companion.getKey
-import com.lagradost.cloudstream3.CloudStreamApp.Companion.setKey
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 
 @CloudstreamPlugin
 class FamilyPornPlugin : Plugin() {
     override fun load(context: Context) {
-        registerMainAPI(FamilyPorn())
+        // Registering the provider and extractor
+        registerMainAPI(FamilyPornProvider())
         registerExtractorAPI(FamilyPornExtractor())
-
-        this.openSettings = { ctx ->
-            val activity = ctx as AppCompatActivity
-            val frag = FamilyPornSettingsFragment()
-            frag.show(activity.supportFragmentManager, "familyporn_settings")
-        }
-    }
-
-    companion object {
-        var cfCookies: String
-            get() = getKey("FAMILYPORN_CF_COOKIES") ?: ""
-            set(value) { setKey("FAMILYPORN_CF_COOKIES", value) }
-
-        var cfUserAgent: String
-            get() = getKey("FAMILYPORN_CF_USER_AGENT") ?: ""
-            set(value) { setKey("FAMILYPORN_CF_USER_AGENT", value) }
-
-        var cfCookieHost: String
-            get() = getKey("FAMILYPORN_CF_COOKIE_HOST") ?: ""
-            set(value) { setKey("FAMILYPORN_CF_COOKIE_HOST", value) }
     }
 }
