bd2bf357324c9164ecda08f091ee122ec29503db
AC321-beep <charanac4@gmail.com>
2026-09-06 12:42:00 +0530

    Update Extractors.kt

--- FamilyPorn/src/main/kotlin/com/familyporn/Extractors.kt
+++ FamilyPorn/src/main/kotlin/com/familyporn/Extractors.kt
@@ -30,7 +30,7 @@ class FamilyPornExtractor : ExtractorApi() {
     }
 
     private suspend fun fetchFireplayer(url: String, referer: String?, callback: (ExtractorLink) -> Unit) {
-        val videoid = url.substringAfter("/video/").substringBefore("?")
+        val videoid = url.trimEnd('/').substringAfter("/video/").substringBefore("?")
         val posturl = "https://watchstreamhd.com/player/index.php?data=$videoid&do=getVideo"
         val headers = mapOf(
             "Accept" to "*/*",
@@ -42,9 +42,11 @@ class FamilyPornExtractor : ExtractorApi() {
         
         val response = FamilyPornProvider.appPost(url = posturl, data = mapOf("hash" to videoid, "r" to (referer ?: "")), headers = headers).text
         val json = AppUtils.parseJson<FireResponse>(response)
-        val link = json.securedlink ?: json.videosource
+        
+        // Added fallback file property just in case API shifts response structure
+        val link = json.securedlink ?: json.videosource ?: json.file
         if (link != null) {
-            callback(newExtractorLink(source = "Fireplayer", name = "Fireplayer", url = link, type = ExtractorLinkType.M3U8) {
+            callback(newExtractorLink(source = "Fireplayer", name = "Fireplayer", url = link, type = ExtractorLinkType.M3U8, quality = Qualities.Unknown.value) {
                 this.referer = "https://watchstreamhd.com/"
                 this.headers = mapOf("Origin" to "https://watchstreamhd.com")
             })
@@ -52,7 +54,7 @@ class FamilyPornExtractor : ExtractorApi() {
     }
 
     private suspend fun fetchVideoStreamingWorld(url: String, referer: String?, callback: (ExtractorLink) -> Unit) {
-        val data = url.substringAfterLast("/")
+        val data = url.trimEnd('/').substringAfterLast("/")
         val posturl = "https://videostreamingworld.com/player/index.php?data=$data&do=getVideo"
         
         val headers = mapOf(
@@ -65,13 +67,16 @@ class FamilyPornExtractor : ExtractorApi() {
         
         val response = FamilyPornProvider.appPost(url = posturl, data = emptyMap(), headers = headers).text
         val video = AppUtils.parseJson<Video>(response)
-        callback(newExtractorLink(source = "VideoStreamingWorld", name = "VideoStreamingWorld", url = video.videoSource, type = ExtractorLinkType.M3U8) {
-            this.referer = "https://videostreamingworld.com/"
-        })
+        
+        if (video.videoSource != null) {
+            callback(newExtractorLink(source = "VideoStreamingWorld", name = "VideoStreamingWorld", url = video.videoSource, type = ExtractorLinkType.M3U8, quality = Qualities.Unknown.value) {
+                this.referer = "https://videostreamingworld.com/"
+            })
+        }
     }
 
     private suspend fun fetchBestWish(url: String, referer: String?, callback: (ExtractorLink) -> Unit) {
-        val data = url.substringAfterLast("/")
+        val data = url.trimEnd('/').substringAfterLast("/")
         val getUrl = "https://bestwish.lol/ajax/stream?filecode=$data"
         
         val headers = mapOf(
@@ -83,15 +88,24 @@ class FamilyPornExtractor : ExtractorApi() {
         
         val response = FamilyPornProvider.appGet(url = getUrl, headers = headers).text
         val stream = AppUtils.parseJson<Stream>(response)
-        callback(newExtractorLink(source = "BestWish", name = "BestWish", url = stream.streaming_url, type = ExtractorLinkType.M3U8) {
-            this.referer = "https://bestwish.lol/"
-        })
+        
+        if (stream.streaming_url != null) {
+            callback(newExtractorLink(source = "BestWish", name = "BestWish", url = stream.streaming_url, type = ExtractorLinkType.M3U8, quality = Qualities.Unknown.value) {
+                this.referer = "https://bestwish.lol/"
+            })
+        }
     }
 
+    // @JsonProperty completely guards data classes against Release APK Obfuscation bugs
     data class FireResponse(
         @JsonProperty("securedLink") val securedlink: String? = null,
-        @JsonProperty("videoSource") val videosource: String? = null
+        @JsonProperty("videoSource") val videosource: String? = null,
+        @JsonProperty("file") val file: String? = null
     )
-    data class Video(val videoSource: String)
-    data class Stream(val streaming_url: String)
+    data class Video(
+        @JsonProperty("videoSource") val videoSource: String? = null
+    )
+    data class Stream(
+        @JsonProperty("streaming_url") val streaming_url: String? = null
+    )
 }
